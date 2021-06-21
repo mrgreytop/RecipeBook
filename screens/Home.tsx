51c775c8097db71f551e27664b96cc630fee13a8
mrgreytop <36601066+mrgreytop@users.noreply.github.com>
2021-06-21 21:10:22 +0100

    init  homescreen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, {useState} from 'react';
+import { View, FlatList } from 'react-native';
+import RecipeCard from '../components/RecipeCard';
 
 
 export default function HomeScreen() {
+
+    const [recipes, setRecipes] = useState<any>()
+
     return (
         <View>
-
+            <FlatList
+                data = {recipes}
+                renderItem = {(p)=>{
+                    return <RecipeCard {...p}/>
+                }}
+                keyExtractor={item=>item.id}
+            >
+            </FlatList>
         </View>
     )
 }
