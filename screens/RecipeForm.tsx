54d7e36237c2348d334cec0214da450438e9cd6d
mrgreytop <36601066+mrgreytop@users.noreply.github.com>
2021-07-23 21:04:57 +0100

    starting rework of ingredient form

--- screens/RecipeForm.tsx
+++ screens/RecipeForm.tsx
@@ -1,14 +1,13 @@
 import React, {useState} from "react";
 import { View, StyleSheet, FlatList, TextInput as NativeTextInput } from "react-native";
-import { TextInput, FAB } from "react-native-paper";
-import IngredientCard from "../components/IngredientCard";
+import { TextInput } from "react-native-paper";
 import { RecipeIngredient } from "../types";
 
 export default function RecipeFormScreen(){
 
     const [name, setName] = useState<string>("");
     const [servings, setServings] = useState<string>("");
-    const [ingredients, setIngredients] = useState<RecipeIngredient[]>();
+    const [ingredients, setIngredients] = useState<RecipeIngredient[]>([]);
 
     return (
         <View style = {styles.flexCol}>
@@ -18,6 +17,7 @@ export default function RecipeFormScreen(){
                 value={name}
                 onChangeText={(text) => setName(text)}
             ></TextInput>
+            
             <TextInput
                 label="servings"
                 value={servings}
@@ -28,18 +28,13 @@ export default function RecipeFormScreen(){
                         keyboardType="number-pad"
                     ></NativeTextInput>
                 }
-                style={{ flex: 1 }}
             ></TextInput>
-            <FlatList
-                data = {ingredients}
-                renderItem = {(item)=>(
-                    <IngredientCard {...item}></IngredientCard>
-                )}
-            ></FlatList>
-            <FAB
-                icon = "plus"
-                style = {styles.fab}
-            ></FAB>
+            <View style ={{flex:1}}>
+                {/* <FlatList
+                    data = {ingredients}
+                >
+                </FlatList> */}
+            </View>
         </View>
     )
 }
@@ -48,11 +43,5 @@ const styles = StyleSheet.create({
     flexCol:{
         flex:1,
         flexDirection:"column"
-    },
-    fab: {
-        position: 'absolute',
-        margin: 16,
-        right: 0,
-        bottom: 0,
     }
 })
