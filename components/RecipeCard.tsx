60ba110b7a7363321aa331fca0eb947e02d76326
mrgreytop <36601066+mrgreytop@users.noreply.github.com>
2021-07-25 11:20:43 +0100

    improved Recipe Card design

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,28 +1,60 @@
 import React from "react";
 import { View } from "react-native";
 import {
-    Text, Chip, Button
+    Headline, Chip, Button, Card
 } from "react-native-paper"
 
 export default function RecipeCard(props:any){
     const ing = props.item
     const tags = ing.tags === undefined ? []:ing.tags;
     return (
-        <View style = {{flex:1, flexDirection:"column"}}>
-            <View>
-                <Text style = {{flex:1}}>{ing.name}</Text>
-                <Button>Edit</Button>
-            </View>
-            <View style = {{flex:1}}>
-                <View style={{ flex: 1, flexWrap: "wrap" }}>
-                    {tags.map((t:any)=>{
-                        return <Chip 
-                            mode = "flat"
-                        >{t.name}</Chip>
-                    })}
-                </View>
-                <Button>Delete</Button>
-            </View>
-        </View>
+        <Card>
+            <Card.Title title = {ing.name}></Card.Title>
+            <Card.Content>
+                <View style={{ flex: 1, flexWrap: "wrap"}}>
+                     {tags.map((t:any)=>{
+                         return <Chip 
+                             mode = "flat"
+                         >{t.name}</Chip>
+                     })}
+                 </View>
+            </Card.Content>
+            <Card.Actions>
+                <Button icon="pencil">Edit</Button>
+                <Button icon="trash-can">Delete</Button>
+            </Card.Actions>
+        </Card>
+        // <View style = {{flex:1, flexDirection:"column"}}>
+        //     <View>
+        //         <Headline style = {{flex:1}}>{ing.name}</Headline>
+        //     </View>
+        //     <View style = {{flex:1}}>
+        //         <View style={{ flex: 1, flexWrap: "wrap", minHeight:30 }}>
+        //             {tags.map((t:any)=>{
+        //                 return <Chip 
+        //                     mode = "flat"
+        //                 >{t.name}</Chip>
+        //             })}
+        //         </View>
+        //     </View>
+        //     <View>
+        //         <List.Section>
+        //             <List.Accordion title="Actions">
+        //                 <List.Item 
+        //                     title = "Edit" 
+        //                     left = {(props)=>(
+        //                         <List.Icon icon = "pencil"/>
+        //                     )}
+        //                 />
+        //                 <List.Item 
+        //                     title = "Delete"
+        //                     left={(props) => (
+        //                         <List.Icon icon="trash-can" />
+        //                     )}
+        //                 />
+        //             </List.Accordion>
+        //         </List.Section>
+        //     </View>
+        // </View>
     )
 }
