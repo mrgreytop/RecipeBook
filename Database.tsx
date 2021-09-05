22c9160c919a338844d09941a705bc965ed75118
mrgreytop <36601066+mrgreytop@users.noreply.github.com>
2021-09-05 17:03:13 +0100

    added unit conversion, unit initialisation

--- Database.tsx
+++ Database.tsx
@@ -1,8 +1,28 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import {Recipe, Unit} from "./types";
-
+import {Recipe, RecipeIngredient, Unit, SiMeasure} from "./types";
 // TODO add shopping lists to database
 
+
+function convert_unit(amount:number, unit:Unit, new_unit:Unit):number|null{
+    if (unit.measure != new_unit.measure){
+        return null
+    }
+    if (unit.measure == SiMeasure.None){
+        return amount
+    }
+    return amount / (unit.factor * new_unit.factor)
+}
+
+function combine_ingredients(ingredients: RecipeIngredient[][]){}
+
+
+export const null_unit = { symbol: "", measure: SiMeasure.None }
+
+const default_units: Unit[] = [
+    null_unit,
+    {symbol: "g", measure:SiMeasure.Weight, factor: 1},
+    {symbol: "ml", measure:SiMeasure.Volume, factor: 1},
+]
 
 export interface IRecipeDatabase{
     getAllRecipes:()=>Promise<Recipe[]>,
@@ -10,11 +30,17 @@ export interface IRecipeDatabase{
     removeRecipe:(recipe_id:number)=>Promise<void>,
     updateRecipe:(recipe_id:number, update:Partial<Recipe>)=>Promise<void>,
     readRecipe: (recipe_id:number)=>Promise<Recipe>,
+    readUnit: (unit_symbol:string)=>Promise<Unit>
 }
 
 export var RecipeDatabase = (async function(new_recipe_listners?: Function[]){
     console.log("initialising database")
     var max_key = await findMaxKey();
+    var unit_init = await checkUnitsInit();
+
+    if (!unit_init){
+        await initUnits(default_units);
+    }
 
     async function findMaxKey():Promise<number>{
         return AsyncStorage.getItem("@maxkey:recipe").then((val:string|null)=>{
@@ -22,6 +48,27 @@ export var RecipeDatabase = (async function(new_recipe_listners?: Function[]){
             console.log("found max_key")
             return max_key
         })    
+    }
+
+    async function checkUnitsInit():Promise<boolean>{
+        return AsyncStorage.getItem("@init:unit").then((val)=>{
+            return (val === null) || (val === "False")
+        })
+    }
+
+    async function writeUnit(unit:Unit){
+        let string_unit = JSON.stringify(unit)
+        return AsyncStorage.setItem(`@unit:${unit.symbol}`, string_unit)
+    }
+
+    async function initUnits(units: Unit[]){
+        let unit_writes = []
+        for (let i = 0; i < units.length; i++) {
+            unit_writes.push(writeUnit(default_units[i]))
+        }
+        return Promise.all(unit_writes).then(()=>{
+            AsyncStorage.setItem("@init:unit", "True")
+        })
     }
 
     async function incrementMaxKey():Promise<number>{
@@ -101,17 +148,19 @@ export var RecipeDatabase = (async function(new_recipe_listners?: Function[]){
             })
         },
 
-        // readUnit: async function(unit_symbol:string): Promise<Unit>{
-        //     let slug_symbol = unit_symbol.replace(" ", "_")
-        //     return AsyncStorage.getItem(`@unit:${slug_symbol}`).then((unit_json:string|null)=>{
-        //         if (unit_json === null) {
-        //             throw new Error(`Cannot find list`);
-        //         } else {
-        //             let unit: Unit = JSON.parse(unit_json);
-        //             return unit;
-        //         }
-        //     })
-        // }
+        readUnit: async function(unit_symbol:string): Promise<Unit>{
+            let slug_symbol = unit_symbol.replace(" ", "_")
+            return AsyncStorage.getItem(`@unit:${slug_symbol}`).then((unit_json:string|null)=>{
+                if (unit_json === null) {
+                    throw new Error(`Cannot find list`);
+                } else {
+                    let unit: Unit = JSON.parse(unit_json);
+                    return unit;
+                }
+            })
+        },
+        
+        writeUnit: writeUnit
     }
 
 })
