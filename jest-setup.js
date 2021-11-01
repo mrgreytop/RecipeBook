ab68d662cdb82c4838b1383b1fb840190e5f4478
mrgreytop <36601066+mrgreytop@users.noreply.github.com>
2021-11-01 14:25:31 +0000

    start over!

--- jest-setup.js
+++
@@ -1,6 +0,0 @@
-import MockAsyncStorage from "mock-async-storage";
-
-const mockStoreImpl = new MockAsyncStorage
-jest.mock('@react-native-async-storage/async-storage', () => mockStoreImpl);
-
-jest.mock('react-native/Libraries/Animated/src/NativeAnimatedHelper');
